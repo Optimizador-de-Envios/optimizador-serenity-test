eaf08b4605eea0a8fa673feb831aacaa9b06d1ef
Nahuel Lemes <nahulem@gmail.com>
2026-04-08 04:28:47 -0300

    feat: create serenity template

---
+++ src/test/java/org/nahulemes/util/TestData.java
@@ -0,0 +1,10 @@
+package org.nahulemes.util;
+
+public final class TestData {
+
+    // Insertar aquí las constantes de datos de prueba, por ejemplo:
+    // public static final String YAHOO_URL = "https://espanol.yahoo.com/";
+
+    private TestData() {
+    }
+}
